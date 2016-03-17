339f8d72882a51ad43f620e22a4348edb337ebad
Joachim Kret <joachim.kret@performgroup.com>
2016-03-17 19:20:03 +0100

    Added playground with third party libraries.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+import XCPlayground
+import Alamofire
+import RxCocoa
+import RxSwift
+
+// Playgorund execution flow: 
+//XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+//XCPlaygroundPage.currentPage.needsIndefiniteExecution = false
+
+var str = "Hello, playground"
